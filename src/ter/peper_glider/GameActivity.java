44fe72c30f74b871cfb92900300df3e636384ed7
schapeyroux <chapeyroux.sebastien@gmail.com>
2013-02-21 15:16:42 +0100

    modif manifest & gameActivity

--- src/ter/peper_glider/GameActivity.java
+++ src/ter/peper_glider/GameActivity.java
@@ -1,18 +1,17 @@
 package ter.peper_glider;
-
+ 
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
-
+ 
 public class GameActivity extends Activity
 implements SensorEventListener {
-	
+
 	SensorManager sm = null;
 	TextView tvGero;
 	
@@ -26,40 +25,37 @@ implements SensorEventListener {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-
+	
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		int sensor = event.sensor.getType();
 		float [] values = event.values;
-		
-        synchronized (this) {
-          if (sensor == Sensor.TYPE_ORIENTATION) {
-        	  float direction_x = values[0];
-        	  float direction_y = values[1];
-        	  float direction_z = values[2];
-        	  tvGero.setText("x: "+String.valueOf(direction_x)+" y: "+String.valueOf(direction_y)+" z : "+String.valueOf(direction_z));
-          }
+
+		synchronized (this) {
+			if (sensor == Sensor.TYPE_ORIENTATION) {
+				float direction_x = values[0];
+        	  	float direction_y = values[1];
+        	  	float direction_z = values[2];
+        	  	tvGero.setText("x: "+String.valueOf(direction_x)+" y: "+String.valueOf(direction_y)+" z : "+String.valueOf(direction_z));
+			}
         }
 	}
 	
-	 protected void onResume() {
-	        super.onResume();
-	        sm.registerListener(this, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+	protected void onResume() {
+		super.onResume();
+		sm.registerListener(this, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	protected void onStop() {
-	    sm.unregisterListener(this, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION));
-	    super.onStop();
+		sm.unregisterListener(this, sm.getDefaultSensor(Sensor.TYPE_ORIENTATION));
+		super.onStop();
 	}
-
 }
