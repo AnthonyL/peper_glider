6893dc7b166cfa3d9866fa68823ab462e0db41da
steven <bernard_steven@hotmail.fr>
2013-02-21 15:51:36 +0100

    paper

--- src/ter/peper_glider/GameActivity.java
+++ src/ter/peper_glider/GameActivity.java
@@ -7,7 +7,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 public class GameActivity extends Activity
@@ -16,12 +19,41 @@ implements SensorEventListener {
 	SensorManager sm = null;
 	TextView tvGero;
 	
+	LayoutParams par;
+	ImageView leaf;
+	int widthView;
+	int posCenter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
 		setContentView(R.layout.activity_game);
+		
 		tvGero = (TextView) findViewById(R.id.tvGero);
+		
+		Display display = getWindowManager().getDefaultDisplay(); 
+		int widthScreen = display.getWidth();  // deprecated height of game
+		int heightScreen = display.getHeight();  // deprecated width of game
+
+		leaf = (ImageView)findViewById(R.id.imageView1);
+		
+
+		int heightView = leaf.getDrawable().getIntrinsicHeight();
+		widthView = leaf.getDrawable().getIntrinsicWidth();
+		tvGero.setText(heightView + " " + widthView);
+		
+		int centerWidth = widthScreen / 2;
+		posCenter = centerWidth - (widthView/2);
+		//tvGero.setText(widthScreen + " " + heightScreen);// w = 320 ; h = 480
+		par = (LayoutParams)leaf.getLayoutParams();
+		
+		par.leftMargin = posCenter;
+		par.topMargin = 10;
+
+		leaf.setLayoutParams(par);
+		leaf.invalidate();
+		
 	}
 
 	@Override
@@ -47,7 +79,20 @@ implements SensorEventListener {
         	  float direction_x = values[0];
         	  float direction_y = values[1];
         	  float direction_z = values[2];
-        	  tvGero.setText("x: "+String.valueOf(direction_x)+" y: "+String.valueOf(direction_y)+" z : "+String.valueOf(direction_z));
+        	  //tvGero.setText("x: "+String.valueOf(direction_x)+" y: "+String.valueOf(direction_y)+" z : "+String.valueOf(direction_z));
+        	  par = (LayoutParams)leaf.getLayoutParams();
+      		
+        	  int pos = par.leftMargin;
+        	  tvGero.setText(((Float)direction_z).intValue() + "toto : "+pos);
+        	  
+        	  if (pos>=320 || pos<=0){
+        		  tvGero.setText("bordure : "+pos);
+        		  par.leftMargin =((Float)direction_z).intValue();
+  				
+  				
+  				leaf.setLayoutParams(par);
+  				leaf.invalidate();
+        	  }
           }
         }
 	}
